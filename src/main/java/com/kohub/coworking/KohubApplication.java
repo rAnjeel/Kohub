49a01d9d47664d74d804ff4d5c0151d80b9de622
Angelo RABEARISON <151778901+rAnjeel@users.noreply.github.com>
2025-02-23 14:47:26 +0300

    Creation du corps du projet
    Structuration des fichiers et des dossiers. Utilisation de Springboot et de Maven

---
+++ src/main/java/com/kohub/coworking/KohubApplication.java
@@ -0,0 +1,13 @@
+package com.kohub.coworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kohub/coworking/KohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kohub.coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
